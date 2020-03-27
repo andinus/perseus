3dee7955670274b92ad8b3931e6c36995f1ee418
Andinus <andinus@nand.sh>
2020-03-27 21:20:12 +0530

    Add registration handler

--- cmd/perseus/main.go
+++ cmd/perseus/main.go
@@ -1,10 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"tildegit.org/andinus/perseus/handler/web"
 	"tildegit.org/andinus/perseus/storage"
 )
 
 func main() {
 	db := storage.Init()
 	defer db.Conn.Close()
+
+	envPort, exists := os.LookupEnv("PERSEUS_PORT")
+	if !exists {
+		envPort = "8080"
+	}
+	addr := fmt.Sprintf("127.0.0.1:%s", envPort)
+
+	srv := &http.Server{
+		Addr:         addr,
+		WriteTimeout: 8 * time.Second,
+		ReadTimeout:  8 * time.Second,
+	}
+
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		web.HandleRegister(w, r, db)
+	})
+
+	log.Printf("main/main.go: listening on port %s...", envPort)
+	log.Fatal(srv.ListenAndServe())
 }
---
+++ handler/web/page.go
@@ -0,0 +1,12 @@
+package web
+
+import "html/template"
+
+// Page holds page information
+type Page struct {
+	SafeList []template.HTML
+	List     []string
+	Error    []string
+	Success  []string
+	Notice   []string
+}
